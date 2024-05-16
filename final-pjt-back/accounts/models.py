068f61f3c926a23725cdf09e970f0f6b966d2c29
허세령 <seryoii@naver.com>
2024-05-16 19:53:01 +0900

    add: 모델 추가

--- final-pjt-back/accounts/models.py
+++ final-pjt-back/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from allauth.account.adapter import DefaultAccountAdapter
+from financial_products.models import SavingProduct
+from financial_products.models import DepositProduct
 
 # Create your models here.
+
+class User(AbstractUser):
+    username = models.CharField(max_length=10)
+    age = models.IntegerField()
+    profile_img = models.ImageField(upload_to='image/', default='image/default_profile.png')
+    salary = models.IntegerField()
+    wealth = models.IntegerField()
+    tendency = models.CharField(max_length=5)
+    desirePeriod = models.IntegerField()
+    saving = models.ManyToManyField(SavingProduct, related_name='interest_users')
+    deposit = models.ManyToManyField(DepositProduct, related_name='interest_users')
