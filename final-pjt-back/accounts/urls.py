daffea3d9df307e8726649b6d2c8fd67e4337c87
허세령 <seryoii@naver.com>
2024-05-20 22:49:01 +0900

    comment: 알고리즘 추천 url 설명

--- final-pjt-back/accounts/urls.py
+++ final-pjt-back/accounts/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('<str:username>/', views.mypage),
     path('profile/<str:username>/', views.user_profile),
+    # 상품 별 관심 있는 유저 불러오기 (시간 오래 걸림. 기다려야함)
     path('get/interest/', views.get_interest),
     path('<int:user_id>/interest/', views.personal_interest)
 ]
--- final-pjt-back/financial_products/urls.py
+++ final-pjt-back/financial_products/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
+# 초기 상품 데이터 생성하기
   path('get_deposit_products/', views.get_deposit_products),
   path('get_saving_products/', views.get_saving_products),
   
+# deposit
   path('deposit/', views.deposit_product_list),
   path('deposit/<str:deposit_name>/', views.deposit_detail),
   path('deposit/<str:deposit_code>/option/', views.deposit_option_list),
@@ -14,8 +16,10 @@ urlpatterns = [
   # path('deposit/month/<int:month>/', views.deposit_month),
   path('like/deposit/<str:deposit_code>/', views.like_deposit),
   path('recommend/deposit/', views.deposit_recommend),
+  # 나이로 상품 추천 (반환 값: 상품 데이터의 id 값)
   path('recommend/deposit/second/', views.deposit_recommend_second),
 
+# saving
   path('saving/', views.saving_product_list),
   path('saving/<str:saving_name>/', views.saving_detail),
   path('saving/<str:saving_name>/option/', views.saving_option_list),
@@ -25,6 +29,7 @@ urlpatterns = [
   # path('saving/month/<int:month>/', views.saving_month),
   path('like/saving/<str:saving_code>/', views.like_saving),
   path('recommend/saving/', views.saving_recommend),
+  # 나이로 상품 추천 (반환 값: 상품 데이터의 id 값)
   path('recommend/saving/second/', views.saving_recommend_second),
 
 
