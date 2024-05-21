f0d9b7c74d286b0d102b957260ab92260143a0f5
허세령 <seryoii@naver.com>
2024-05-21 17:55:56 +0900

    add: recommend store 추가

---
+++ final-pjt-front/src/stores/recommend.js
@@ -0,0 +1,32 @@
+import { defineStore } from "pinia";
+import { ref, computed } from "vue";
+import { useRouter } from "vue-router";
+import axios from "axios";
+import swal from "sweetalert";
+import { useUserStore } from "./user";
+import { lightFormatters } from "date-fns";
+
+export const useRecommendStore = defineStore('recommend', () => {
+    const API_URL = "http://127.0.0.1:8000/products";
+    const recommendFirst = ref([])
+    const recommendSecond = ref([])
+    const userStore = useUserStore()
+    const getRecommendFirst = function () {
+        axios({
+        method: 'get',
+        url: `${API_URL}/recommend/deposit/`,
+        headers: {
+            Authorization: userStore.token
+        }
+        })
+        .then((response) => {
+            console.log(response)
+            recommendFirst.value.push(response.data.id)
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+    }
+    return { getRecommendFirst, recommendFirst }
+}, { persist: true }
+);
--- final-pjt-front/src/views/RecommendView.vue
+++ final-pjt-front/src/views/RecommendView.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script setup>
-
+import { useRecommendStore } from '@/stores/recommend';
+import { onMounted } from 'vue'
+const store = useRecommendStore()
+onMounted(() => {
+    store.getRecommendFirst()
+})
+console.log(store.recommendFirst)
 </script>
 
 <style scoped>
